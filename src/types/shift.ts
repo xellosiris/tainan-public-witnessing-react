a5a6c215e32e75dda9bf8f2e54b621141163d4f6
Huang Kuan Wei <xellosiris@gmail.com>
2026-01-03 18:23:49 +0800

    新增 shift expiredAt

--- src/types/shift.ts
+++ src/types/shift.ts
@@ -5,13 +5,14 @@ const shiftSchema = z
     id: z.uuid(),
     active: z.boolean(),
     date: z.string().nonempty(),
+    yearMonth: z.string(),
     siteUuid: z.uuid(),
     startTime: z.string().nonempty(),
     endTime: z.string().nonempty(),
     attendees: z.uuid().array(),
-    hasStatistic: z.boolean().optional(),
     attendeesLimit: z.number().positive(),
     requiredDeliverers: z.number().positive().default(2),
+    expiredAt: z.date(),
   })
   .refine((data) => data.attendees.length <= data.attendeesLimit, {
     message: "參與人員的總人數不能超過參與上限",
